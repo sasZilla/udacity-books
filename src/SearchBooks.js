bb8c89f074c563b520a219fa038a4ca505b23fa8
sasZilla <sas.smail.sas@gmail.com>
2017-07-29 12:28:51 +0300

    feat: use search API to load books

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,27 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
+import debounce from 'lodash/debounce'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import BookItem from './BookItem'
 
-class SearchBooks extends React.Component {
+const SEARCH_LIMIT = 10
+
+class SearchBooks extends Component {
+  state = {
+    isLoading: false,
+    query: '',
+    books: []
+  }
+
+  constructor() {
+    super()
+    this.searchBooks = debounce(this.searchBooks, 1000)
+  }
+
+  searchBooks(query) {
+    const queryStr = query.trim()
+    this.setState({'isLoading': true})
+    BooksAPI.search(queryStr, SEARCH_LIMIT).then((books) => {
+      this.setState({
+        books,
+        query: queryStr,
+        isLoading: false
+      })
+    })
+  }
+
+  updateQuery(query) {
+    this.setState({ query })
+    if (query.length > 0) {
+      this.searchBooks(query)
+    }
+  }
+
   render() {
+    const books = this.state.books
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          {this.state.isLoading ?
+            <h2>Loading...</h2>  :
+            <ol className="books-grid">
+              {books.map((book) => (
+                <li key={book.id}>
+                  <BookItem book={book} />
+                </li>
+              ))}
+            </ol>
+          }
         </div>
       </div>
     )
