f63f03b7fa3445eaff46955f94245f837e2f8d71
sasZilla <sas.smail.sas@gmail.com>
2017-07-30 16:30:36 +0300

    chore: eslint, auto code prettifier

--- src/BookItem.js
+++ src/BookItem.js
@@ -1,30 +1,42 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import * as BooksAPI from './BooksAPI'
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as BooksAPI from './BooksAPI';
 
 class BookItem extends React.Component {
   state = {
     shelf: this.props.book.shelf
-  }
+  };
 
   onSelect(shelf) {
-    this.setState({shelf: shelf})
-    this.props.book.shelf = shelf
+    this.setState({ shelf: shelf });
+    this.props.book.shelf = shelf;
     BooksAPI.update(this.props.book, shelf).then(() => {
-      this.props.onChangeShelf(this.props.book)
-    })
+      this.props.onChangeShelf(this.props.book);
+    });
   }
 
   render() {
-    const book = this.props.book
+    const book = this.props.book;
 
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+ book.imageLinks.thumbnail + ')' }}></div>
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: 'url(' + book.imageLinks.thumbnail + ')'
+            }}
+          />
           <div className="book-shelf-changer">
-            <select onChange={(event) => this.onSelect(event.target.value)} value={this.state.shelf}>
-              <option value="none" disabled>Move to...</option>
+            <select
+              onChange={event => this.onSelect(event.target.value)}
+              value={this.state.shelf}
+            >
+              <option value="none" disabled>
+                Move to...
+              </option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
@@ -32,18 +44,20 @@ class BookItem extends React.Component {
             </select>
           </div>
         </div>
-        <div className="book-title">{book.title}</div>
+        <div className="book-title">
+          {book.title}
+        </div>
         <div className="book-authors">
           {book.authors.join(', ')}
         </div>
       </div>
-    )
+    );
   }
 }
 
 BookItem.propTypes = {
   book: PropTypes.object.isRequired,
   onChangeShelf: PropTypes.func.isRequired
-}
+};
 
-export default BookItem
+export default BookItem;
--- src/BooksList.js
+++ src/BooksList.js
@@ -1,46 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import * as BooksAPI from './BooksAPI'
-import BooksShelf from './BooksShelf'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import BooksShelf from './BooksShelf';
 
 class BooksList extends React.Component {
   state = {
     books: []
-  }
+  };
 
   componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
   }
 
   onChangeShelf(book) {
-    this.setState((state) => ({
+    this.setState(state => ({
       books: state.books.map(function(b) {
         if (b.id === book.id) {
           b.shelf = book.shelf;
         }
         return b;
       })
-    }))
+    }));
   }
 
   render() {
-    const books = this.state.books
+    const books = this.state.books;
     const bookShelfs = [
       {
         title: 'Currently Reading',
-        books: books.filter((b) => b.shelf === 'currentlyReading')
+        books: books.filter(b => b.shelf === 'currentlyReading')
       },
       {
         title: 'Want to Read',
-        books: books.filter((b) => b.shelf === 'wantToRead')
+        books: books.filter(b => b.shelf === 'wantToRead')
       },
       {
         title: 'Read',
-        books: books.filter((b) => b.shelf === 'read')
+        books: books.filter(b => b.shelf === 'read')
       }
-    ]
+    ];
 
     return (
       <div className="list-books">
@@ -49,21 +49,22 @@ class BooksList extends React.Component {
         </div>
         <div className="list-books-content">
           <div>
-            {bookShelfs.map((shelf,i) => (
-              <BooksShelf key={shelf.title + '-' + i}
+            {bookShelfs.map((shelf, i) =>
+              <BooksShelf
+                key={shelf.title + '-' + i}
                 books={shelf.books}
                 title={shelf.title}
                 onChangeShelf={this.onChangeShelf.bind(this)}
-                />
-            ))}
+              />
+            )}
           </div>
         </div>
         <div className="open-search">
           <Link to="/search">Add a book</Link>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default BooksList
+export default BooksList;
